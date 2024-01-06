37a36ce5e9c0f211171a57f33cdd4017f96a1d82
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-06 17:54:05 -0600

    practiced user input

---
+++ 2-userInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+main(){
+
+
+std::string name;    
+
+std::cout<< "What is your name?" << '\n';
+std::cin>> name;
+std::cout<< "I see, so your name is " << name;
+
+
+
+    return 0;
+}
