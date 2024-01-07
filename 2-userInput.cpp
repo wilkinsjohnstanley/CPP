86e3d7ef961baaba33206478b4074b8633b47fb4
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-06 18:28:16 -0600

    user input

--- 2-userInput.cpp
+++ 2-userInput.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-
 int main(){
 
 std::string name;
 
 std::cout<<"What is your name?" << '\n';
 std::cin>>name;
-std::cout<<"Hey " << name << "! It's been a while."<<'\n' <<"It's so good to see you again.";
-
+std::cout<<"Hey " << name << "! How's it been going?";
 
 
     return 0;
