feb568183738ab58a2751563e2820b3bf3526f37
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-08 15:31:40 -0600

    cmath functions. yes.

---
+++ 13-cmath.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+
+int main(){
+
+double decimal = 2.6;
+int x = 5;
+int y = 25;
+
+// std::cout<< sqrt(y);
+// std::cout<< round(decimal);
+std::cout<< pow(2, 2);
+
+
+
+    return 0;
+}
