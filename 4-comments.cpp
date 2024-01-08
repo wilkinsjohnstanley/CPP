016b2331e3fa50808cdb313ad697825e3b443ab9
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-08 11:40:09 -0600

    today we learned about comments

---
+++ 4-comments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//this is the main method
+int main(){
+    /*
+    I can simply right whatever I need to right here.e
+    And it takes up multiple lines without a problem.
+    That's wonderful!
+    */
+
+
+    return 0;
+}
