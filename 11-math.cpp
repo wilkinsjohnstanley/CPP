88d3e165532c8ff78d0f3dafe15fa4e5d1f3894f
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-08 13:56:49 -0600

    did math

---
+++ 11-math.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+
+int x = 2;
+int y = 2;
+// add +    subtract -     divide  /    multiply    *    
+//find the remainder  %    increment   ++    decrement   --
+
+std::cout<< --x ; // x-1 = 
+
+
+return 0;
+}
