a9a964829571111165a1957c46229bbd94335970
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-10 15:45:57 -0600

    first commit of the day. learned about comparison operators!

---
+++ 17-comparison.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+/*
+== equal to
+!= not equal to
+> greater than
+< less than
+>= greater than equal to
+<= less than equal to
+*/
+
+
+if (1 >= 2){
+    std::cout<<"True";
+} else 
+    std::cout<<"False";
+}
