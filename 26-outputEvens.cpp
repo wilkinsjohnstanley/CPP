93713177c556e3ba8219686ec076344e4c519cfe
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-12 19:05:48 -0600

    yes

---
+++ 26-outputEvens.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+for (int i = 0; i < 11; i++) {
+
+//if odd
+if(i%2==1){
+
+    //skip
+    continue;
+
+}
+
+
+  std::cout << i << "\n";
+} 
+
+
+
+    return 0;
+}
