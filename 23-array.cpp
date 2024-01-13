83b22c0741c3ab6282b6a53480eb34e159e1fdfb
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-12 18:36:21 -0600

    more stuff

---
+++ 23-array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(){
+
+int numbers[]={1, 2, 3, 4, 5};
+std::string fruits[]={"apple", "grape", "banana"};
+
+std::cout<<numbers[2] << '\n' << fruits[1];
+
+    return 0;
+}
---
+++ 24-accessingElementsOfAnArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+int main(){
+
+int nums[4]={1, 2, 3, 4};
+
+for(int i; i<4; i++){
+   std::cout<<nums[i]<<'\n'; 
+   Sleep(1000);
+}
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+  
