8979a7307e2cd175bed1c82a960988125e1998d1
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-12 20:29:21 -0600

    Gotta catch 'em all!

---
+++ 28-stringarrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main() 
+{
+std::string pokemon[6]={"Pikachu", "Butterfree", "Pidgeotto", "Charmander", "Bulbasaur", "Squirtle"};
+
+for(int x=0; x<6;x++){
+    std::cout<<pokemon[x]<<'\n';
+}
+
+} 
