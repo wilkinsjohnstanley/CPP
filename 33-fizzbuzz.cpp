2f77d8428204dda33e571fea21b6fa2faa50f9ee
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-18 17:09:19 -0600

    buzzfizz

--- 33-fizzbuzz.cpp
+++ 33-fizzbuzz.cpp
@@ -2,17 +2,6 @@
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-
-
-
-/*
- * Complete the 'fizzBuzz' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
-
 void fizzBuzz(int n) {
 
 for(int i = 1; i<=n; i++){
@@ -41,34 +30,12 @@ else if (!(i%3==0 || i%5==0)){
 
 int main()
 {
-    string n_temp;
-    getline(cin, n_temp);
 
-    int n = stoi(ltrim(rtrim(n_temp)));
-
+int n;
+cout<<"Enter a number: "<<'\n';
+cin>>n;
     fizzBuzz(n);
 
     return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
